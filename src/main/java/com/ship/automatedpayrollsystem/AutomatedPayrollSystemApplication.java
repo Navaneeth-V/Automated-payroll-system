c126e9f733be5ce66a28c465824e21f1b944751f
Navaneeth <162485789+Navaneeth-V@users.noreply.github.com>
2025-09-07 20:16:12 +0530

    initial commit

---
+++ src/main/java/com/ship/automatedpayrollsystem/AutomatedPayrollSystemApplication.java
@@ -0,0 +1,13 @@
+package com.ship.automatedpayrollsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutomatedPayrollSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AutomatedPayrollSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ship/automatedpayrollsystem/AutomatedPayrollSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ship.automatedpayrollsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutomatedPayrollSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
