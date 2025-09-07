d71b86567cede1619dc100afc61a9a4252bc847e
Navaneeth <162485789+Navaneeth-V@users.noreply.github.com>
2025-09-07 21:00:12 +0530

    Employee model

--- src/main/java/com/ship/automatedpayrollsystem/AutomatedPayrollSystemApplication.java
+++ src/main/java/com/ship/automatedpayrollsystem/AutomatedPayrollSystemApplication.java
@@ -8,6 +8,7 @@ public class AutomatedPayrollSystemApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(AutomatedPayrollSystemApplication.class, args);
+        System.out.println("Hello World!");
     }
 
 }
---
+++ src/main/java/com/ship/automatedpayrollsystem/model/Employee.java
@@ -0,0 +1,87 @@
+package com.ship.automatedpayrollsystem.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "employee")
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "phone")
+    private String phone;
+
+    @Column(name = "department")
+    private String department;
+
+    @Column(name = "designation")
+    private String designation;
+
+    @Column(name = "salary")
+    private Integer salary;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public String getDesignation() {
+        return designation;
+    }
+
+    public void setDesignation(String designation) {
+        this.designation = designation;
+    }
+
+    public Integer getSalary() {
+        return salary;
+    }
+
+    public void setSalary(Integer salary) {
+        this.salary = salary;
+    }
+}
