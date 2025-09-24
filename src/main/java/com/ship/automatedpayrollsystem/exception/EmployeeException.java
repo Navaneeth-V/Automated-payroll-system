93226b1d0765594a76e869da81d2d31ee6abf67f
Navaneeth <162485789+Navaneeth-V@users.noreply.github.com>
2025-09-24 23:39:09 +0530

    exception handler

--- src/main/java/com/ship/automatedpayrollsystem/exception/EmployeeException.java
+++ src/main/java/com/ship/automatedpayrollsystem/exception/EmployeeException.java
@@ -1,4 +1,15 @@
 package com.ship.automatedpayrollsystem.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
 public class EmployeeException {
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<Object> employeeExceptionHandler(Exception exception) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(exception.getMessage());
+    }
 }
